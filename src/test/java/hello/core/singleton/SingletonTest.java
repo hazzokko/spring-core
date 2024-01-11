5f531721de18fe0a5c7879316c467e3a85f9e040
hazzokko <hazzokko@gmail.com>
2024-01-11 22:08:58 +0900

    Test : 스프링 없는 순수한 DI 컨테이너

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,6 +2,7 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,7 @@ public class SingletonTest {
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
     void pureContainer() {
         AppConfig appConfig = new AppConfig();
+
         // 1. 조회 : 호출할 때마다 객체를 생성
         MemberService memberService1 = appConfig.memberService();
 
@@ -21,5 +23,7 @@ public class SingletonTest {
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
+        // memberService1 != memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
     }
 }
