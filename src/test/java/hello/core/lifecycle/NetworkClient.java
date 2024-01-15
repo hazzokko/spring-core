5223077419bea271ab6c2bae9204da7365c64c49
hazzokko <hazzokko@gmail.com>
2024-01-15 20:21:36 +0900

    8. 빈 생명주기 콜백

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,31 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작 시 호출
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    // 연결이 된 상태에서 연결한 서버에 메시지 전송
+    public void call(String message) {
+        System.out.println("call : " + url + " / message = " + message);
+    }
+
+    // 서비스 종료 시 호출
+    public void disconnect() {
+        System.out.println("close : " + url);
+    }
+}
