5b9c86f0d37de1e3854950c263163eaa5faee6d2
hazzokko <hazzokko@gmail.com>
2024-01-19 21:36:33 +0900

    8. Scope And Proxy

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider; // MyLogger를 찾을 수 있는 DL 할 수 있는 객체가 주입
+    private final MyLogger myLogger;
 
     // HttpServletRequest : 자바에서 제공하는 표준 서블릿 규약에 의한 Http Request 정보를 받을 수 있음
     @RequestMapping("log-demo")
@@ -21,7 +21,8 @@ public class LogDemoController {
     public String logDemo(HttpServletRequest request) {
         // 스프링 인터셉터나 서블릿 필터에서 공통처리 하는 것이 좋음
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject(); // HTTP 요청이 오면서 처음으로 달라고 하면 이 때 생성됨
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
